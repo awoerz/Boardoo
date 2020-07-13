13fc106d7a7f82a4bc9456c912dbd950fd6053e4
Adam <azreow@gmail.com>
2020-07-13 18:12:48 -0400

    Added card component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,27 @@
 import React, {useState} from 'react';
+import Card from './Card';
 
 export default function Board(props) {
+    const [cards, setCards] = useState([
+        {
+            id: 1,
+            title: 'This is card 1'
+        },
+        {
+            id: 2, 
+            title: 'This is card 2'
+        }
+    ])
+
+
     return (
         <div className="board-container">
             <h1 className="board-title">{props.title}</h1>
+            <div className="card-container">
+                {cards.map(card => {
+                    return <Card key={card.id} title={card.title} />
+                })}
+            </div>
             <div className='board-button-container'>
                 <button className='board-button'>+</button>
             </div>
---
+++ src/components/Card.js
@@ -0,0 +1,7 @@
+import React, {useState} from 'react';
+
+export default function Card(props) {
+    return (
+        <h1>{props.title}</h1>
+    )
+}
